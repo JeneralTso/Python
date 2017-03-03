96f576c3cc5232dba7e2a1bdc0ba842a64a5fb96
JeneralTso <jennytso789@gmail.com>
2017-03-03 04:39:59 -0800

    Python range function drill

---
+++ rangeDrill.py
@@ -0,0 +1,21 @@
+'''
+Jenny Tso
+Range Drill
+
+'''
+
+print("   PYTHON RANGE DRILL\n")
+print("1. Use the range() function with one parameter to display '0, 1, 2, 3':")
+i = 0
+for i in range(4):
+    print(i)
+
+print("\n2. Use the range() function with 3 parameters to display '3, 2, 1, 0':")
+for i in range(3, -1, -1):
+    print(i)
+
+print("\n3. Use the range() function with 3 parameters to display '8, 6, 4, 2':")
+for i in range(8, 0, -2):
+    print(i)
+
+print("\n\n**jazz hands**")
